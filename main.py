43d9c0382e25c089f5a3b682382467198e01f0a6
poonkuzhali <poonkuzhali1996@gmail.com>
2023-10-29 13:04:48 -0400

    Re-arranged algorithm order

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ from kamada_kawai import kk_layout
 
 
 def main():
-    G = nx.cubical_graph()
-    kk_layout(G)
+    # G = nx.cubical_graph()
+    G = nx.karate_club_graph()
     fruchterman_reingold(G)
     plt.close()
     nx.draw_networkx(G, pos=nx.kamada_kawai_layout(G), node_size=10, width=0.1, with_labels=False)
     plt.savefig("orig.png")
+    plt.close()
+    nx.draw_networkx(G, pos=nx.spring_layout(G), node_size=10, width=0.1, with_labels=False)
+    plt.savefig("orig2.png")
+    kk_layout(G)
 
 
 if __name__ == "__main__":
