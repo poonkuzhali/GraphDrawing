dfe54035d5aa4336ccb13d1f0657a6e0061ef4e6
Shreeraksha-98 <87537866+Shreeraksha-98@users.noreply.github.com>
2023-10-29 20:07:24 -0400

    Added Force Atlas implementation

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import networkx as nx
 
 from fruchterman_reingold import fruchterman_reingold
 from kamada_kawai import kk_layout
-
+from force_atlas import force_atlas_layout
 
 def main():
-    # G = nx.cubical_graph()
+    #G = nx.cubical_graph()
     G = nx.karate_club_graph()
     fruchterman_reingold(G)
+    force_atlas_layout(G, gravity = 1.0)
     plt.close()
     nx.draw_networkx(G, pos=nx.kamada_kawai_layout(G), node_size=10, width=0.1, with_labels=False)
-    plt.savefig("orig.png")
+    plt.savefig("nx_kk.png")
     plt.close()
     nx.draw_networkx(G, pos=nx.spring_layout(G), node_size=10, width=0.1, with_labels=False)
-    plt.savefig("orig2.png")
+    plt.savefig("nx_fr.png")
     kk_layout(G)
-
 
 if __name__ == "__main__":
     main()
