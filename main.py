99ef3abc1c97786df205b5821340daee9dfdb901
poonkuzhali <poonkuzhali1996@gmail.com>
2023-12-15 15:44:39 -0500

    Final changes

--- main.py
+++ main.py
@@ -10,12 +10,6 @@ def main():
     try:
         G = nx.cubical_graph()
         print(f"Number of nodes: {nx.number_of_nodes(G)}")
-        plt.close()
-        nx.draw_networkx(G, pos=nx.kamada_kawai_layout(G), node_size=10, width=0.1, with_labels=False)
-        plt.savefig("nx_kk.png")
-        plt.close()
-        nx.draw_networkx(G, pos=nx.spring_layout(G), node_size=10, width=0.1, with_labels=False)
-        plt.savefig("nx_fr.png")
         input_num = int(input("Enter a choice: 1 for FR, 2 for KK, 3 for FA, 0 to exit:  "))
         # flag = bool(input("Do you want to print the images:(True or False)  "))
         while input_num != 0:
